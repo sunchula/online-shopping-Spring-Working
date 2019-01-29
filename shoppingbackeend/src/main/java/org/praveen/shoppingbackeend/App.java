caf953ceb62b342d9304d1630a4917977b413120
sunchula <sunchulapraveen@gmail.com>
2019-01-29 15:29:09 +0530

    Initiall Commit - Project Set up.

---
+++ shoppingbackeend/src/main/java/org/praveen/shoppingbackeend/App.java
@@ -0,0 +1,13 @@
+package org.praveen.shoppingbackeend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
