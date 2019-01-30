1c976c90762935df0ee93eaf598ed23a89499799
sunchula <sunchulapraveen@gmail.com>
2019-01-30 05:48:59 +0530

    Added Bootstrap Un styled Page and added to our Spring application

--- onlineShoppingSpring/src/main/java/org/praveen/onlineShoppingSpring/controller/PageController.java
+++ onlineShoppingSpring/src/main/java/org/praveen/onlineShoppingSpring/controller/PageController.java
@@ -1,7 +1,9 @@
 package org.praveen.onlineShoppingSpring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -9,10 +11,32 @@ public class PageController {
 
 	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
-
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting","Welcome to Spring MVC!!");
+		mv.addObject("greeting", "Welcome to Spring MVC!!");
+		return mv;
+	}
+
+	/*Example for RequestParam
+	@RequestMapping(value = "/test")
+	public ModelAndView test(@RequestParam(value = "greeting", required = false) String greeting) {
+		if (greeting == null) {
+			greeting = "Hello There!!";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
 		return mv;
 
-	}
+	}*/
+
+	/* Example for @PathVariable 
+	@RequestMapping(value = "/test/{greeting}")
+	public ModelAndView test1(@PathVariable("greeting") String greeting) {
+		if (greeting == null) {
+			greeting = "Hello There!!";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+
+	}*/
 }
